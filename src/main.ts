a292ff7870164adbab4f0fa7c9942774afd57769
gshong <hgs33hgs@gmail.com>
2020-12-20 00:55:40 +0900

    밝기 조정

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,19 @@ import jimp from 'jimp';
 import path from 'path';
 import mkdirp from 'mkdirp';
 
-if (process.argv.length < 4) {
-    console.warn('./image_manager.exe [filename] [extname] [length = 1] [startCnt = 0]');
+if (process.argv.length >= 3 && (process.argv[2] === '--help' || process.argv[2] === '-h')) {
+    console.warn('./image_manager.exe [filename = test] [extname = jpg] [length = 1] [startCnt = 0]');
     process.exit(0);
 }
 
-const filename: string = process.argv[2];
-const extname: string = process.argv[3];
+let filename: string = 'test';
+if (process.argv.length >= 3) {
+    filename = process.argv[2];
+}
+let extname: string = 'jpg';
+if (process.argv.length >= 4) {
+    extname = process.argv[3];
+}
 let fileLength: number = 1;
 if (process.argv.length >= 5) {
     fileLength = Number(process.argv[4]);
@@ -21,7 +27,7 @@ let saveCnt: number = startCnt;
 
 console.log('filename : ' + filename + ', fileLength : ' + fileLength);
 
-async function changeImageData(index: number, brightUp: boolean) {
+async function changeImageData(index: number, brightType: number = 0) {
     return new Promise(async (resolve, reject) => {
         let loadFilename: string = filename + ` (${index}).${extname}`;
         let saveFilename: string = filename + ` (${saveCnt++}).${extname}`;
@@ -32,21 +38,29 @@ async function changeImageData(index: number, brightUp: boolean) {
                 reject(err);
             }
 
-            let brightness: number = -0.3;
-            if (brightUp) {
-                brightness = 0.3;
-            }
-
-            lenna
-                .brightness(brightness) //INFO: 밝기 -1~1
-                .contrast(0.4) //INFO: 대조 -1~1
-                .resize(416, 416)
-                .write(path.join('./result', saveFilename), err => {
+            let brightness: number = -0.1;
+            if (brightType !== 0) {
+                if (brightType === 1) {
+                    brightness = 0.4;
+                }
+                lenna
+                    .brightness(brightness) //INFO: 밝기 -1~1
+                    .contrast(0.4) //INFO: 대조 -1~1
+                    .resize(416, 416)
+                    .write(path.join('./result', saveFilename), err => {
+                        if (err) {
+                            reject(err);
+                        }
+                        resolve();
+                    });
+            } else {
+                lenna.resize(416, 416).write(path.join('./result', saveFilename), err => {
                     if (err) {
                         reject(err);
                     }
                     resolve();
                 });
+            }
         });
     });
 }
@@ -55,13 +69,17 @@ async function main() {
     try {
         await mkdirp('./result');
         for (let i = 0; i < fileLength; i++) {
-            await changeImageData(i, true);
-            console.log(`Image Processing (Bright Up) Finish(${i + 1}/${fileLength})`);
+            await changeImageData(i, 0);
+            console.log(`Image Processing (Original) Finish(${i + 1}/${fileLength})`);
         }
         for (let i = 0; i < fileLength; i++) {
-            await changeImageData(i, false);
-            console.log(`Image Processing (Bright Down) Finish(${i + 1}/${fileLength})`);
+            await changeImageData(i, 1);
+            console.log(`Image Processing (Bright Up) Finish(${i + 1}/${fileLength})`);
         }
+        // for (let i = 0; i < fileLength; i++) {
+        //     await changeImageData(i, false);
+        //     console.log(`Image Processing (Bright Down) Finish(${i + 1}/${fileLength})`);
+        // }
     } catch (ex) {
         console.error(ex);
         process.exit(0);
